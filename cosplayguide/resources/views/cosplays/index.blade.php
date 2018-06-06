751e35e266a92e581f2691c0095de058c870c4a5
Phedra Moerloos <phedra.moerloos@student.kdg.be>
2018-06-07 00:29:59 +0200

    profiel info css toegevoegd

--- cosplayguide/resources/views/cosplays/index.blade.php
+++ cosplayguide/resources/views/cosplays/index.blade.php
@@ -5,44 +5,74 @@
 
 
   <div class="row cosplay-profiel">
-    <div class="col-3">
+    <div class="col-3 profiel-info">
 
-      <div>
-          <a id="navbarDropdown" class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-              . . .<!--<img class="social-facebook" src="/img/social-icon.png" alt="Facebook icoon">-->
-          </a>
 
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="/profiel/edit">Edit profiel</a>
-              <a class="dropdown-item" href="{{ route('logout') }}"
-                 onclick="event.preventDefault();
-                               document.getElementById('logout-form').submit();">
-                  {{ __('Logout') }}
+
+      <div class="profiel">
+
+
+        <div class="row justify-content-end">
+
+            <div class="col-4">
+              <a id="navbarDropdown" class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                  <img class="more-icon" src="/img/iconen/more-full-lichter.svg" alt="toon meer">
               </a>
 
-              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                  @csrf
-              </form>
-          </div>
+
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="/profiel/edit">Edit profiel</a>
+                  <a class="dropdown-item" href="{{ route('logout') }}"
+                     onclick="event.preventDefault();
+                                   document.getElementById('logout-form').submit();">
+                      {{ __('Logout') }}
+                  </a>
+
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      @csrf
+                  </form>
+              </div>
+            </div>
+
+        </div>
+
+        <p class="profile-title">PROFIEL</p>
+        <img class="profile-picture-large" src="{{ asset('/storage/images/' . $user->profile_picture_url) }}" alt="profile picture {{ $user->username }}">
+
+        <h1>{{ $user->username }}</h1>
+        <p class="profile-description">{{ $user->description }}</p>
+
       </div>
 
 
-      <img src="{{ asset('/storage/images/' . $user->profile_picture_url) }}" alt="profile picture {{ $user->username }}">
 
-      <h1>{{ $user->username }}</h1>
-
-      <p>{{ $user->description }}</p>
+      <div class="level-info">
 
 
-      <p>number of completed cosplays: {{ $number_of_completed_cosplays }}</p>
-      <p>number of cosplays in progress: {{ $number_of_cosplays_in_progress }}</p>
+        <div class="row justify-content-center">
 
-      <div class="level">
-        <div class="background-circle"></div>
-        <div class="level-circle"></div>
-        <!--<img class="background-circle" src="/img/level/level-basis-cirkel.png" alt="level cirkel">-->
-        <img class="level-circle" src={{"/img/level/level-" . $user->level . ".png"}} alt="level {{ $user->level }}">
-        <p class="level-title">{{ $user->level }}</p>
+          <div class="col-4">
+            <div class="progress-circle"></div>
+            <p>IN PROGRESS</p>
+            <p>{{ $number_of_cosplays_in_progress }}</p>
+          </div>
+
+          <div class="col-4">
+            <div class="completed-circle"></div>
+            <p>COMPLETED</p>
+            <p>{{ $number_of_completed_cosplays }}</p>
+          </div>
+
+        </div>
+
+
+
+        <div class="level">
+          <div class="background-circle"></div>
+          <img class="level-circle" src={{"/img/level/level-" . $user->level . ".png"}} alt="level {{ $user->level }}">
+          <p class="level-title">{{ $user->level }}</p>
+        </div>
+
       </div>
 
 
@@ -61,7 +91,8 @@
 
               @if ($cosplay->status == "completed")
                 <div class="col-4 cosplay">
-                  <img class="img-fluid" src="{{ asset('/storage/images/' . $cosplay->thumbnail_url) }}" alt="thumbnail {{ $cosplay->name }} ">
+
+                  <a class="cosplay-hover-link" href="#"><img class="img-fluid" src="{{ asset('/storage/images/' . $cosplay->thumbnail_url) }}" alt="thumbnail {{ $cosplay->name }} "></a>
 
                   <div class="cosplay-hover">
                     <img class="cosplay-overlay" src="/img/cosplay-overlay.png">
@@ -82,7 +113,7 @@
 
               @else
               <div class="col-4 cosplay">
-                <img class="img-fluid desaturate" src="{{ asset('/storage/images/' . $cosplay->thumbnail_url) }}" alt="thumbnail {{ $cosplay->name }} ">
+                <a class="cosplay-hover-link" href="#"><img class="img-fluid desaturate" src="{{ asset('/storage/images/' . $cosplay->thumbnail_url) }}" alt="thumbnail {{ $cosplay->name }} "></a>
 
                 <div class="cosplay-hover">
                   <img class="cosplay-overlay" src="/img/cosplay-overlay.png">
