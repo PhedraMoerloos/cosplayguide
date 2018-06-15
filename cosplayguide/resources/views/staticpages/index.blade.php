bdb74560910fcc5854d1890fc61fcafc9378240f
Phedra Moerloos <phedra.moerloos@student.kdg.be>
2018-06-15 14:23:43 +0200

    Toegevoegd home pagina header zonder slider

--- cosplayguide/resources/views/staticpages/index.blade.php
+++ cosplayguide/resources/views/staticpages/index.blade.php
@@ -1,7 +1,48 @@
 @extends('master')
 
+@section('title')
+  <title>Wordt jouw favoriete personage bij Cosplay Guide</title>
+@endsection
+
 @section('content')
-  <h1>Become your favorite character</h1>
+
+  <div class="home">
+
+
+    <div class="row header">
+      <div class="col-4">
+
+        <div class="home-header-top">
+
+          <div class="titel">
+            <h1 class="large purple">BECOME YOUR FAVORITE CHARACTER</h1>
+            <div class="titels-underline small purple"></div>
+          </div>
+
+
+          <div class="join-knop">
+            <a href="/register">START JOUW COSPLAY</a>
+            <a href="/register"><img src="/img/join-knop.png" alt="Knop met plus teken"></a>
+          </div>
+
+        </div>
+
+        <p class="bold">Bekijk nog meer inspirerende cosplays op:</p>
+        <img src="/img/deviantart.png" alt="Deviantart website logo">
+
+      </div>
+
+      
+
+      <div class="col-7">
+        <img src="/img/howling-banshee.jpg" alt="slider">
+      </div>
+
+    </div>
+
+
+  </div>
+
 @endsection
 
 
