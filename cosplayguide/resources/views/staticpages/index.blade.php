15dd73188eea97056a8fbd309ef3a310696d6706
Phedra Moerloos <phedra.moerloos@student.kdg.be>
2018-06-15 16:25:27 +0200

    Toegevoegd home slider met controls, tot aan randen

--- cosplayguide/resources/views/staticpages/index.blade.php
+++ cosplayguide/resources/views/staticpages/index.blade.php
@@ -32,13 +32,72 @@
 
       </div>
 
-      
+
 
       <div class="col-7">
-        <img src="/img/howling-banshee.jpg" alt="slider">
+
+        <div id="carouselHome" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+
+
+
+            <div class="carousel-item active">
+
+              <img class="d-block" src="/img/howling-banshee.jpg" alt="Foto Howling Banshee cosplay door Arga Lifestream">
+              <div class="carousel-caption d-none d-md-block">
+                <p>1 of 3</p>
+                <a href="#">HOWLING BANSHEE</a>
+              </div>
+
+
+            </div>
+
+
+            <div class="carousel-item">
+
+              <img class="d-block" src="/img/night-elf.jpg" alt="Foto Night Elf cosplay door Arga Lifestream">
+              <div class="carousel-caption d-none d-md-block">
+                <p>2 of 3</p>
+                <a href="#">NIGHT ELF</a>
+              </div>
+
+
+            </div>
+
+
+            <div class="carousel-item">
+
+              <img class="d-block" src="/img/wolverine.jpg" alt="Foto Wolverine cosplay">
+              <div class="carousel-caption d-none d-md-block">
+                <p>3 of 3</p>
+                <a href="#">WOLVERINE</a>
+              </div>
+
+
+            </div>
+
+
+
+          </div>
+
+          <a class="carousel-control-prev progress" href="#carouselHome" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next progress" href="#carouselHome" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+
+        </div>
+
       </div>
 
     </div>
+
+
+
+
 
 
   </div>
